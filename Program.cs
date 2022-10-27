4ce226d1791e9f2270bb973b335e4110bf562034
Khoa00 <116774975+Khoa00@users.noreply.github.com>
2022-10-27 09:07:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thuctap_Share
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
